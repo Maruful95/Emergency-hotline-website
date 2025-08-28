0d9accb9c2795b5e3c1b01de26c2530a1ba8acd8
Maruful95 <maruful94955@gmail.com>
2025-08-29 00:06:14 +0600

    added hotline number, copy and call button functionality

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-// function to get number
-function getNumber(id) {
-    const Number = parseInt(document.getElementById(id).innerText)
-     return Number
+const callData = []
+
+// function to get text
+function getText(id) {
+    const text = document.getElementById(id).innerText
+     return text
 }
 
-// function to set value
-function setValue(value) {
-    document.getElementById("heart-count").innerText = value
+// function to get number
+function getNumber(id) {
+    const number = parseInt(document.getElementById(id).innerText)
+     return number
 }
+
 
 
 // heart icon functionality
@@ -17,9 +21,60 @@ document.querySelectorAll(".heart-icon").forEach(function(icon) {
         const heartCount = getNumber("heart-count")
         const newHeartCount = heartCount + 1
 
-        setValue(newHeartCount)
-    });
-});
+        document.getElementById("heart-count").innerText = newHeartCount
+    })
+})
 
 
-// hotline number functionality
+// hotline number copy on click
+document.querySelectorAll(".hotline-number").forEach(function (text) {
+    text.addEventListener("click", function () {
+        const hotline = text.innerText.trim()
+        navigator.clipboard.writeText(hotline)
+    })
+})
+
+// copy button functionality
+const button = document.querySelectorAll(".copy-button")
+const hotline = document.querySelectorAll(".hotline-number")
+
+button.forEach(function (btn, index) {
+    btn.addEventListener("click", function () {
+        const copyHotline = btn.parentElement.parentElement.querySelector(".hotline-number").innerText.trim();
+        navigator.clipboard.writeText(copyHotline)
+
+        alert("Number copied: " + copyHotline)
+
+        const copyCount = getNumber("copy-count")
+        const newCopyCount = copyCount + 1
+        document.getElementById("copy-count").innerText = newCopyCount
+    })
+})
+
+// call button functionality
+document.querySelectorAll(".call-button").forEach(function(btn) {
+    btn.addEventListener("click", function (e) {
+
+        const coinCount = getNumber("coin-count")
+        if (coinCount < 20) {
+            alert("You don't have enough coins. You need at least 20 coins.")
+            return
+        }
+        const serviceName = btn.parentElement.parentElement.querySelector(".service-name").innerText.trim()
+        const hotlineNumber = btn.parentElement.parentElement.querySelector(".hotline-number").innerText.trim()
+        alert("Calling " + serviceName + ": " + hotlineNumber)
+
+        const newCoinCount = coinCount - 20
+
+        document.getElementById("coin-count").innerText = newCoinCount
+
+        // send data to history
+        const name = btn.parentElement.parentElement.querySelector(".name").innerText.trim()
+        const data = {
+            name:name,
+            date:new Date().toLocaleTimeString()
+        }
+        callData.push(data)
+    })
+})
+
